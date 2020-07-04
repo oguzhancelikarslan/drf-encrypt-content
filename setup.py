8fd0a13e08ba58308fc3a59f79725532ed365f3a
Oğuzhan Çelikarslan <oguzhancelikarslan@gmail.com>
2020-07-04 17:06:01 +0300

    added extra definitions to setup.py file

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
+    description=(
+        'The package helps you encrypt your serialized data.'
+    ),
+    long_description=long_description,
+    long_description_content_type="test/markdown",
     install_requires=[
         'cryptography>=2.9.2',
     ],
